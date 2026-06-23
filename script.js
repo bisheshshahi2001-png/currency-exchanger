b63d7ae3f4b2a0fd1651bd9d4ce4617cb40a4ac3
bishesh-shahi <bisheshshahi2001@gmail.com>
2026-06-23 06:18:03 +0545

    Complete currency exchanger project

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/
 
 const dropdowns = document.querySelectorAll('.drop-down select')
 const btn = document.querySelector('.done')
-
-
+const fromCurr = document.querySelector('.from select')
+const toCurr = document.querySelector('.to select')
+const msg = document.querySelector('.msg')
 
 
 for(let select of dropdowns){
@@ -34,9 +35,27 @@ const updateFlage = (Element) => {
 };
 
 
-btn.addEventListener('click', (e) =>{
-    e.preventDefault();
-    let amount = document.querySelector('.input input').value;
-    
+const updateExchangeRate =  async () =>{
+    let amount = document.querySelector('.input input');
+    let amtval = amount.value;
+    if(amtval === '' || amtval < 1){
+        amount.value = 1;
+        amtval = 1;
+    }
+    const URL = `${BASE_URL}${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
 
+    let total = amtval * rate ;
+    msg.innerText =  ` ${amtval} ${fromCurr.value}  = ${total} ${toCurr.value}` ;
+
+};
+
+btn.addEventListener('click' ,  (e) => {
+     e.preventDefault(); 
+    updateExchangeRate();
+})
+window.addEventListener('load', (e) => {
+    updateExchangeRate();
 })
